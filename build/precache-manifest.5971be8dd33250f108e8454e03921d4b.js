f8bc88bdd71863db42b9314d8741aca97098bc73
weiweihsu <monij.hsu@gmail.com>
2019-05-08 08:53:47 -0700

    initial commit

---
+++ build/precache-manifest.5971be8dd33250f108e8454e03921d4b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "95f8a31e9f3c2dc50dc50970071aa543",
+    "url": "/index.html"
+  },
+  {
+    "revision": "339d430f6defdf814610",
+    "url": "/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "339d430f6defdf814610",
+    "url": "/static/js/main.cc764552.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
